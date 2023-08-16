41909845a06ff48f2be0a08151168a120daac201
Albert Kim <alberthsk115@gmail.com>
2023-08-15 17:05:24 -0700

    added couple more variables into post model

--- app/models/post.py
+++ app/models/post.py
@@ -8,5 +8,7 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+    picture = db.Column(db.String)
+    categories = db.Column(db.String)
     title = db.Column(db.String(100))
     content = db.Column(db.String(500))
