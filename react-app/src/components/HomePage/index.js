be2f557c8e16792ca76359f74e431b959862bbc8
Albert Kim <alberthsk115@gmail.com>
2023-10-11 18:04:10 -0700

    home page content displayed

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useEffect } from "react";
 import { getAllPostThunk } from "../../store/post";
+import './homepage.css'
 
 
 function HomePage() {
@@ -15,14 +16,20 @@ function HomePage() {
         dispatch(getAllPostThunk())
     }, [dispatch])
 
+    if (!user) history.push('/')
 
     return (
         <div className="home-page-container">
             <div className="post-contents">
                 {posts.map((post) => (
-                    <div className="titles">
-                        <h1>{post.title}</h1>
+                    <div className="individual-posts">
+                        <h2 className="post-title">{post.title}</h2>
                         <img className='post-images' src={post.images} />
+                        <p>{post.categories}</p>
+                        <p>{post.content}</p>
+                        <p>{post.location}</p>
+                        <p>{post.createdAt.slice(0, 17)}</p>
+                        <button onClick={() => alert("Coming Soon!")}>Message Me</button>
                     </div>
                 ))}
             </div>
