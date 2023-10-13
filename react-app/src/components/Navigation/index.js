767eb9043abdd17c595ab97271bed5764e820eac
Albert Kim <alberthsk115@gmail.com>
2023-10-12 18:09:38 -0700

    create post button and slight css work

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -8,15 +8,19 @@ function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	if(sessionUser) <NavLink exact to ='/home'></NavLink>
+
 	return (
 		<div className="navigation-container">
-			<div>
+			<div className="marketIt">
 				<NavLink exact to='/'>MarketIt</NavLink>
 			</div>
-			{isLoaded && (
-				<li>
+			{isLoaded && sessionUser && (
+			<>
+				<button className="post-button">Create Post</button>
+				<div>
 					<ProfileButton user={sessionUser} />
-				</li>
+				</div>
+			</>
 			)}
 		</div>
 	);
